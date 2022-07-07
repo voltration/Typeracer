9f5dbdb105c30b5e08308e1781722ee6179092d9
Sander <69873514+voltration@users.noreply.github.com>
2022-07-07 03:33:39 +0000

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<head>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap" rel="stylesheet">
+</head>
+<body>
+	<h1 class="m-32 inter-900 text-7xl bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-yellow-500">typeracer</h1>
+	<div class="flex items-center justify-center h-screen">
+		
+	</div>
+</body>
+
+<style>
+	.inter-900 {
+		font-family: 'Inter', sans-serif;
+	}
+</style>
