4914d44ba15726509033391c8a11a8f55d9dd718
Sander <69873514+voltration@users.noreply.github.com>
2022-07-07 06:00:39 +0200

    init

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,25 @@
+<script>
+    const url = "https://zenquotes.io/api/quotes/";
+    let quote_txt = "";
+
+    async function quote() {
+
+        const response = await fetch(url, {
+            mode: "no-cors"
+        });
+        quote_txt = await response.json();
+    }
+
+    quote()
+</script>
+
 <head>
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap" rel="stylesheet">
 </head>
 <body>
-	<h1 class="m-32 inter-900 text-7xl bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-yellow-500">typeracer</h1>
+	<h1 class="m-32 inter-900 text-7xl text-gray-400">typeracer</h1>
 	<div class="flex items-center justify-center h-screen">
-		
+		<p id="text">{quote_txt}</p>
 	</div>
 </body>
 
@@ -12,4 +27,7 @@
 	.inter-900 {
 		font-family: 'Inter', sans-serif;
 	}
+    body {
+        background-color: lightgray;
+    }
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -5,17 +5,14 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
+	preprocess: [
+		preprocess({
+		  postcss: true,
+		}),
+	  ],
 
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
